7c5c09b89abec28762a4cb196c876da167315dee
pifupro_iOS_admin <chenyl@pifupro.com>
2015-04-29 20:46:22 +0800

    Initial Commit

---
+++ CYLSearchViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYLSearchViewController
+//
+//  Created by chenyilong on 15/4/29.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CYLSearchViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CYLSearchViewController
+//
+//  Created by chenyilong on 15/4/29.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
